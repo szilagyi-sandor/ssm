66eba6476f91eeef46067c7ce0c5fa398ff94bcb
Sándor Szilágyi <sandormark.szilagyi@nestle.com>
2023-08-08 16:02:30 +0200

    fix: remove done todos

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,11 @@ import { useGetThemeClasses } from '@shared/themes';
 import { router } from './routing';
 import lightBeam from './app.lightBeam.module.scss';
 
-// TODO: #1 use appsettings to set default theme
-// TODO: #1 make sure all files have the CHECKED comment
-// TODO: #1 search for Shapes and change to Triangles
-
-// TODO: #1 theme loader + loading out can be used with delayer nicely
+// TODO: #1 theme loader + loading out can be used with delayer nicely !! -> here
 
 // TODO: #1 finish 0.2.1 check then do 0.2.2 check because imports changed a lot, and also check all the files
-
 // TODO: cv page
+// TODO: fix lazy load blocking
 
 function App() {
   const classes = useGetThemeClasses({
